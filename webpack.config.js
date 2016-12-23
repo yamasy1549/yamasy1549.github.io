4c00b3f481219a4317c7e0e212c5f1dca03901f1
yamasy1549 <sanae@yamasy.info>
2016-12-23 14:48:36 +0900

    Settings of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,41 @@
+const glob = require('glob')
+
 module.exports = {
+  context: __dirname + '/src',
   entry: {
-    jsx: './src/app.jsx',
-    html: './src/index.html'
+    jsx: './index.jsx',
+    html: './index.html',
+    css: './css/style.css',
+    img: glob.sync('./src/img/**/*.*').map(f => f.replace('./src', '.')),
+    font: glob.sync('./src/font/**/*.*').map(f => f.replace('./src', '.')),
   },
   output: {
-    path: './dist',
-    filename: 'bundle.js'
+    path: __dirname + '/dist',
+    filename: 'index.js'
   },
   module: {
     loaders: [
       {
         test: /\.js(x?)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel'
       },
       {
         test: /\.html$/,
         loader: 'file?name=[name].[ext]'
+      },
+      {
+        test: /\.css$/,
+        loader: 'file?name=[path][name].[ext]'
+      },
+      {
+        test: /img\/(.*\/)?.*\.(gif|png|jp(e?)g|svg)$/,
+        loaders: ['file?name=[path][name].[ext]', 'img?minimize']
+      },
+      {
+        test: /font\/.*\.(eot|otf|ttf|woff(2?)|svg(z?))$/,
+        loader: 'file?name=[path][name].[ext]'
       }
     ]
   }
-};
-
+}
