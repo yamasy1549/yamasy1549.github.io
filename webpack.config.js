c5c987e5a5fcdcde822396420a0a97faf2f7547b
yamasy1549 <sanae@yamasy.info>
2016-12-23 10:44:00 +0900

    Prepare for building react

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './src/app.jsx',
+  output: {
+    path: './dist',
+    filename: 'bundle.js'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js(x?)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
+
