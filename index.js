936ea2718da1520d5ff841672da5be94a6763c49
yamasy1549 <sanae@yamasy.info>
2017-03-08 15:50:54 +0900

    build :muscle:

--- index.js
+++ index.js
@@ -10332,7 +10332,7 @@ exports.default = function () {
       ),
       _react2.default.createElement(
         Event,
-        { time: '2016/08~', icons: ['prog-rails', 'html5-01', 'css3-01'] },
+        { time: '2016/08~', icons: ['prog-js01', 'html5-01', 'css3-01', 'prog-rails'] },
         _react2.default.createElement(
           'a',
           { href: 'https://www.animatelab.com/' },
@@ -10342,7 +10342,7 @@ exports.default = function () {
       ),
       _react2.default.createElement(
         Event,
-        { time: '2017/03~', icons: ['html5-01', 'css3-01'] },
+        { time: '2017/03~', icons: ['prog-js01', 'html5-01', 'css3-01'] },
         _react2.default.createElement(
           'a',
           { href: 'http://corp.moneyforward.com/' },
